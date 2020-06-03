7be1b5e9fcc375bf7a60c2b36018aeb1f6f1d5f8
Erwan31 <spilmonterwan@gmail.com>
2020-06-03 23:15:18 +0100

    Moving properly

--- asteroids.js
+++ asteroids.js
@@ -1,9 +1,12 @@
-var canvas = document.getElementById("gameCanvas");
-var context = canvas.getContext("2d");
 const FPS = 30;
 const SHIP_SIZE = 30;
 const TURN_SPEED = 360;
 const SHIP_THRUST = 5;  // acceleration base number in pixels
+const FRICTION = 0.7; // when not accelerating anymore
+
+/** @type {HTMLCanvasElement} */
+var canvas = document.getElementById("gameCanvas");
+var context = canvas.getContext("2d");
 
 var ship = {
     x: canvas.width/2,
@@ -22,52 +25,55 @@ var ship = {
 document.addEventListener("keydown", keyDown);
 document.addEventListener("keyup", keyUp);
 
-function keyDown(e) {
+// Loop
+setInterval( update, 1000 / FPS);
 
-    console.log('KeyDown', e.keyCode)
+// When pressing a key
+function keyDown( /** @type {KeyboardEvent} */ e) {
+
+    //console.log('KeyDown', e.keyCode)
     switch(e.keyCode){
         // Left arrow key -> rotate left
         case 37:
             ship.rot = (TURN_SPEED/180)*Math.PI/FPS;
-        break;
+            break;
 
         // Up arrow key -> propulsion forward
         case 38:
             ship.thrusting = true;
-        break;
+            break;
 
         // Right arrow key -> rotate left
         case 39:
             ship.rot = -(TURN_SPEED/180)*Math.PI/FPS;
-        break;
+            break;
     }
 }
 
-function keyUp(e){
+// After releasing a key
+function keyUp( /** @type {KeyboardEvent} */ e){
     switch(e.keyCode){
         // Left arrow key -> rotate left
         case 37:
             ship.rot = 0;
-        break;
+            break;
 
         // Up arrow key -> propulsion forward
         case 38:
             ship.thrusting = false;
-        break;
+            break;
 
         // Right arrow key -> rotate left
         case 39:
             ship.rot = 0;
-        break;
+            break;
     }
 }
-
-setInterval( update, 1000 / FPS);
 
 function update() {
     // Draw Space
     context.fillStyle = "black";
-    context.fillRect(0, 0, canvas.clientWidth, canvas.height);
+    context.fillRect(0, 0, canvas.width, canvas.height);
 
     // Draw Triangular Spaceship
     context.strokeStyle = "white";
@@ -95,20 +101,60 @@ function update() {
     context.fillStyle = "red";
     context.fillRect(ship.x, ship.y, 1, 1);
 
-    // Rotate Spaceship
-    ship.angle += ship.rot;
-
     // Thrusting
     if(ship.thrusting){
         ship.thrust.x += SHIP_THRUST*Math.cos(ship.angle) / FPS;
         ship.thrust.y -= SHIP_THRUST*Math.sin(ship.angle) / FPS;
+
+        // Little flame behing the ship when thrusting
+        // Draw Triangular shape
+        context.fillStyle = "red";
+        context.strokeStyle = "yellow";
+        context.lineWidth = SHIP_SIZE/50;
+        context.beginPath();
+        // Nose of the Spaceship
+        context.moveTo( 
+            ship.x + 4/3*ship.radius*Math.cos(ship.angle),
+            ship.y - 4/3*ship.radius*Math.sin(ship.angle)
+            );
+        // Left side
+        context.lineTo(
+            ship.x - ship.radius*(2/3*Math.cos(ship.angle) + Math.sin(ship.angle)),
+            ship.y + ship.radius*(Math.sin(ship.angle) - Math.cos(ship.angle))
+        );
+        // Right side
+        context.lineTo(
+            ship.x - ship.radius*(2/3*Math.cos(ship.angle) - Math.sin(ship.angle)),
+            ship.y + ship.radius*(Math.sin(ship.angle) + Math.cos(ship.angle))
+        );
+        context.closePath();
+        context.stroke();
+    
     }
     else{
-        ship.thrust.x = 0;
-        ship.thrust.y = 0;
+        ship.thrust.x -= FRICTION*ship.thrust.x / FPS;
+        ship.thrust.y -= FRICTION*ship.thrust.y / FPS;
     }
+
+    // Rotate Spaceship
+    ship.angle += ship.rot;
 
     // Move SpaceShip
     ship.x += ship.thrust.x;
     ship.y += ship.thrust.y;
+
+    // Handle Edge of the Screen
+    if( ship.x < 0 - ship.radius){
+        ship.x = canvas.width + ship.radius;
+    }
+    else if( ship.x > canvas.width + ship.radius){
+        ship.x = -ship.radius;
+    }
+
+    if( ship.y < 0 - ship.radius){
+        ship.y = canvas.height + ship.radius;
+    }
+    else if( ship.y > canvas.width + ship.radius){
+        ship.y = -ship.radius;
+    }
 }
