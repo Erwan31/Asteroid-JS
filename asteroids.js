c83f0c0adef784a9a1f16883d373f9d839e2c0e6
Erwan31 <spilmonterwan@gmail.com>
2020-06-04 12:04:26 +0100

    Random shape asteroids

--- asteroids.js
+++ asteroids.js
@@ -3,12 +3,16 @@ const SHIP_SIZE = 30;
 const TURN_SPEED = 360;
 const SHIP_THRUST = 5;  // acceleration base number in pixels
 const FRICTION = 0.7; // when not accelerating anymore
+const ASTEROIDS_NUM = 3; // initial asteroids numbers
+const ASTEROID_SPD = 20; // starting speed in pixels/s
+const ASTEROID_SIZE = 100; // in pixels
+const ASTEROID_SIDES = 10; // number of side of each asteroid created
+const ASTEROIDS_IMPERFECTION = 0.4; // 0 none, 1 really imperfect
 
 /** @type {HTMLCanvasElement} */
-var canvas = document.getElementById("gameCanvas");
-var context = canvas.getContext("2d");
-
-var ship = {
+let canvas = document.getElementById("gameCanvas");
+let context = canvas.getContext("2d");
+let ship = {
     x: canvas.width/2,
     y: canvas.height/2,
     radius: SHIP_SIZE/2,
@@ -18,7 +22,49 @@ var ship = {
     thrust: {
         x: 0,
         y: 0
+    },
+    offset: []
+}
+
+
+
+let asteroids = [];
+createAsteroidBelt();
+
+function createAsteroidBelt() {
+    asteroids = [];
+    let x, y;
+
+    for( let i = 0; i < ASTEROIDS_NUM; i++){
+        do{
+            x = Math.floor(Math.random()*canvas.width);
+            y = Math.floor(Math.random()*canvas.height);
+        }while(distBetweenPoints( ship.x, ship.y, x, y ) < (ASTEROID_SIZE*2 + ship.radius));
+        asteroids.push(newAsteroid( x, y));
     }
+}
+
+function distBetweenPoints( shipX, shipY, x, y){
+    return Math.sqrt(Math.pow(shipX - x, 2) + Math.pow(shipY - y, 2));
+}
+
+function newAsteroid( x, y) {
+    let asteroid = {
+        x: x,
+        y, y,
+        xv: Math.random() * ASTEROID_SPD / FPS * (Math.random() < 0.5 ? 1: -1),
+        yv: Math.random() * ASTEROID_SPD / FPS * (Math.random() < 0.5 ? 1: -1),
+        radius: ASTEROID_SIZE / 2,
+        angle: Math.random() * Math.PI * 2,
+        side: Math.floor(Math.random() * (ASTEROID_SIDES + 1) + ASTEROID_SIDES / 2),
+        offset: []
+    }
+
+    for( let i = 0; i < asteroid.side; i++){
+        asteroid.offset.push( Math.random() * ASTEROIDS_IMPERFECTION*2 + 1 - ASTEROIDS_IMPERFECTION);
+    }
+
+    return asteroid;
 }
 
 // Events Listeners
@@ -96,10 +142,44 @@ function update() {
     );
     context.closePath();
     context.stroke();
+
+        // Draw the asteroids
+        context.fillStyle = "grey";
+        context.lineWidth = SHIP_SIZE / 20;
     
-    // Center Dot
-    context.fillStyle = "red";
-    context.fillRect(ship.x, ship.y, 1, 1);
+        let x, y, radius, angle, sides, offset = [];
+    
+        for( var i = 0; i < asteroids.length; i++){
+            // asteroid props
+            x = asteroids[i].x;
+            y = asteroids[i].y;
+            angle = asteroids[i].angle;
+            radius = asteroids[i].radius;
+            sides = asteroids[i].side;
+            offset = asteroids[i].offset;
+    
+            //console.log( x, y, angle, radius, sides);
+    
+            //draw a path
+            context.beginPath();
+            context.moveTo(
+                x + radius*offset[0]*Math.cos(angle),
+                y + radius*offset[0]*Math.sin(angle)
+            )
+            //draw a polygon
+            for( var side_poly = 1; side_poly < sides; side_poly++){
+                context.lineTo(
+                    x + radius*offset[side_poly]*Math.cos( angle + side_poly*Math.PI* 2 / sides),
+                    y + radius*offset[side_poly]*Math.sin( angle + side_poly*Math.PI* 2 / sides),
+                )
+            }
+            context.closePath();
+            context.stroke();
+            
+            //move the asteroid
+    
+            //handle edge of screen
+        }
 
     // Thrusting
     if(ship.thrusting){
@@ -109,7 +189,7 @@ function update() {
         // Little flame behing the ship when thrusting
         // Draw Triangular Spaceship
         context.strokeStyle = "yellow";
-        context.fillStyle = "red"
+        context.fillStyle = "red";
         context.lineWidth = SHIP_SIZE/20;
         context.beginPath();
         // Nose of the Spaceship
@@ -130,10 +210,6 @@ function update() {
         context.closePath();
         context.fill();
         context.stroke();
-        
-        // Center Dot
-        context.fillStyle = "red";
-        context.fillRect(ship.x, ship.y, 1, 1);
     }
     else{
         ship.thrust.x -= FRICTION*ship.thrust.x / FPS;
